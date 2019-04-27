ff7ddb38e4d9ca0c33da7fe52ba8031f89d722b2
Callisto89 <Callisto89@users.noreply.github.com>
2019-04-27 19:51:46 +0200

    Colors
    Moved tab bar colors to Colors.js.

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,9 +1,14 @@
-const tintColor = '#2f95dc';
+const tintColor = '#04A5C9';
+
+/*https://coolors.co/f4d06f-ddfff7-e6af2e-04a5c9-19647e
+Länk till exempel på färgschema*/
 
 export default {
   tintColor,
   tabIconDefault: '#ccc',
   tabIconSelected: tintColor,
+  tabBackgroundDefault: 'gray',
+  tabBackgroundSelected: '#19647e',
   tabBar: '#fefefe',
   errorBackground: 'red',
   errorText: '#fff',
