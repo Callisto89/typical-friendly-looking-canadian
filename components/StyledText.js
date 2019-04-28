72281d6af4b5a887bc270c26776d2257c1d50ab8
Callisto89 <Callisto89@users.noreply.github.com>
2019-04-28 11:40:40 +0200

    Text styles
    Added text styles to component StyledText. Removed styles from LiveScreen.

--- components/StyledText.js
+++ components/StyledText.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-export class MonoText extends React.Component {
+export class StandardText extends React.Component {
   render() {
-    return <Text {...this.props} style={[this.props.style, { fontFamily: 'helvetica' }]} />;
+    return <Text {...this.props} style={[this.props.style, { fontFamily: 'helvetica', fontSize: 17, color: 'tomato', }]} />;
+  }
+}
+
+export class SmallText extends React.Component {
+  render() {
+    return <Text {...this.props} style={[this.props.style, { fontFamily: 'helvetica', fontSize: 10, color: 'tomato', }]} />;
+  }
+}
+
+export class ButtonText extends React.Component {
+  render() {
+    return <Text {...this.props} style={[this.props.style, { fontFamily: 'helvetica', fontSize: 20, color: 'white', }]} />;
   }
 }
