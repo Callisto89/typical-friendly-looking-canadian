a3464479cbde57c7ac3be0b32d9b4acc9854be2c
KittenSoup <freddo@gmail.com>
2019-04-28 15:56:57 +0200

    Refreshing data on navigation

--- screens/UpcomingScreen.js
+++ screens/UpcomingScreen.js
@@ -2,28 +2,40 @@ import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import { getUpcomingEvents } from '../utils/mockData';
+import { NavigationEvents } from 'react-navigation';
 
 export default class UpcomingScreen extends React.Component {
   state = {
     upcomingEvents: [],
   };
-  
+
+  constructor(props) {
+    super(props);
+
+    updateData = () => {
+      getUpcomingEvents()
+        .then(events => this.setState({
+          upcomingEvents: events,
+        }));
+    };
+  }
+
   static navigationOptions = {
     title: 'Upcoming',
   };
 
   componentDidMount() {
     console.log('getting events');
-    return getUpcomingEvents()
-      .then(events => this.setState({
-        upcomingEvents: events,
-      }));
+    updateData();
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
         <View>
+        <NavigationEvents
+          onWillFocus={() => updateData()}
+        />
           {
             this.state.upcomingEvents.map((event, index) => <Text key={index}>{event.name}, {event.startDate}, {event.players}</Text>)
           }
