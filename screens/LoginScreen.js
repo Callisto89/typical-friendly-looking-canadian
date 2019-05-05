296e9cf4bf493b30f73917ab29126ef250e5031d
Fredrik H <freddo@gmail.com>
2019-05-05 14:33:23 +0200

    Added email / password login fields

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -11,58 +11,74 @@ import * as firebase from 'firebase';
 import UserService from '../utils/UserService';
 
 export default class LoginScreen extends React.Component {
-  static navigationOptions = {
-      header: null,
-  };
+    static navigationOptions = {
+        header: null,
+    };
 
-  constructor(props) {
-      super(props);
-      this.state = {};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-  componentDidMount() {
-      firebase.auth().onAuthStateChanged(user => this.setState({
-          user: UserService.getUserData(user)
-      }));
-  }
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => this.setState({
+            user: UserService.getUserData(user)
+        }));
+    }
 
-  handleSignup = (data) => {
-      UserService.createAccount(data);
-  }
+    handleSignup = (data) => {
+        UserService.createAccount(data);
+    }
 
-handleLogin = (data) => {
-    UserService.login(data);
-}
+    handleLogin = (data) => {
+        UserService.login(data);
+    }
 
-render() {
-    return (
-        <View style={styles.contentContainer}>
-            <Text style={styles.getStartedText}>Can&apos;t log in?</Text>
-            <Text style={styles.getStartedText}>You need to register!</Text>
-            <TextInput
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                onChangeText={text => this.setState({ nickname: text })}
-                textContentType='nickname'
-                placeholder='Nickname'
-            />
-            <TextInput
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                onChangeText={text => this.setState({ email: text })}
-                textContentType='emailAddress'
-                placeholder='email'
-            />
-            <TextInput
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                onChangeText={text => this.setState({ password: text })}
-                secureTextEntry
-                textContentType='password'
-                placeholder='password'
-            />
-            <Button onPress={() => this.handleSignup(this.state)} title='Submit' />
+    render() {
+        return (
+            <View style={styles.contentContainer}>
+                <Text style={styles.getStartedText}>Log in</Text>
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={text => this.setState({ email: text })}
+                    textContentType='emailAddress'
+                    placeholder='email'
+                />
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={text => this.setState({ password: text })}
+                    secureTextEntry
+                    textContentType='password'
+                    placeholder='password'
+                />
+                <Button onPress={() => this.handleLogin(this.state)} title='Submit' />
 
-        </View>
-    );
-}
+                <Text style={styles.getStartedText}>Can&apos;t log in?</Text>
+                <Text style={styles.getStartedText}>You need to register!</Text>
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={text => this.setState({ nickname: text })}
+                    textContentType='nickname'
+                    placeholder='Nickname'
+                />
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={text => this.setState({ email: text })}
+                    textContentType='emailAddress'
+                    placeholder='email'
+                />
+                <TextInput
+                    style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={text => this.setState({ password: text })}
+                    secureTextEntry
+                    textContentType='password'
+                    placeholder='password'
+                />
+                <Button onPress={() => this.handleSignup(this.state)} title='Submit' />
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
