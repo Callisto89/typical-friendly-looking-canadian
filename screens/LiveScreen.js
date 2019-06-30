6075594d6914493add30b4803499c29084130f83
Callisto89 <Callisto89@users.noreply.github.com>
2019-06-30 14:14:28 +0200

    Update LiveScreen.js

--- screens/LiveScreen.js
+++ screens/LiveScreen.js
@@ -14,88 +14,88 @@ import Event from '../components/Event';
 import AddEvent from '../components/AddEvent';
 
 export default class LiveScreen extends React.Component {
-  static navigationOptions = {
-      header: null,
-  };
+    static navigationOptions = {
+        header: null,
+    };
 
-  dataRequests = [];
+    dataRequests = [];
 
-  constructor(props) {
-      super(props);
-      this.state = {
-          greeting: '',
-          nick: '',
-          nextEvent: {
-              players: [],
-              waitingList: []
-          }
-      };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            greeting: '',
+            nick: '',
+            nextEvent: {
+                players: [],
+                waitingList: []
+            }
+        };
+    }
 
-  componentDidMount() {
-      console.log('componentdidmount, fetching');
-      this.updateData();
-      fetch('https://europe-west1-wardr-94a12.cloudfunctions.net/helloWorld')
-          .then((res) => {
-              this.setState({
-                  greeting: res._bodyText,
-                  nick: UserService.getUserData().displayName
-              });
-              this.forceUpdate();
-          })
-          .catch(() => 'ERROR');
-  }
+    componentDidMount() {
+        console.log('componentdidmount, fetching');
+        this.updateData();
+        fetch('https://europe-west1-wardr-94a12.cloudfunctions.net/helloWorld')
+            .then((res) => {
+                this.setState({
+                    greeting: res._bodyText,
+                    nick: UserService.getUserData().displayName
+                });
+                this.forceUpdate();
+            })
+            .catch(() => 'ERROR');
+    }
 
-  componentWillUnmount() {
-  }
+    componentWillUnmount() {
+    }
 
-updateData = () => {
-    getUpcomingEvents()
-        .then((events) => {
-            const nextEvent = events[0];
-            this.setState({
-                nextEvent,
+    updateData = () => {
+        getUpcomingEvents()
+            .then((events) => {
+                const nextEvent = events[0];
+                this.setState({
+                    nextEvent,
+                });
             });
-        });
-};
+    };
 
-render() {
-    const { greeting, nick, nextEvent } = this.state;
-    const selectStyle = () => {
-        if (nextEvent.players.includes(nick)) {
+    render() {
+        const { greeting, nick, nextEvent } = this.state;
+        const selectStyle = () => {
+            if (nextEvent.players.includes(nick)) {
+                return {
+                    ...styles.headerContainer,
+                    backgroundColor: Colors.colorPositive
+                };
+            }
             return {
                 ...styles.headerContainer,
-                backgroundColor: Colors.colorPositive
+                backgroundColor: Colors.colorPrimary
             };
-        }
-        return {
-            ...styles.headerContainer,
-            backgroundColor: Colors.colorPrimary
         };
-    };
-    return (
-        <BackgroundImage>
-            <View style={styles.container}>
-                <ScrollView style={styles.container}>
-                    <View style={styles.contentContainer}>
-                        <StandardText key={greeting} style={{ alignSelf: 'center', }}>
-                            { greeting }
-                            { nick }
-                        </StandardText>
-                        <View style={styles.buttonContainer}><AddEvent /></View>
-                        <View style={selectStyle()}>
-                            <BigText>Next event</BigText>
+        return (
+            <BackgroundImage>
+                <View style={styles.container}>
+                    <ScrollView style={styles.container}>
+                        <View style={styles.contentContainer}>
+                            <StandardText key={greeting} style={{ alignSelf: 'center', }}>
+                                {greeting}
+                                {nick}
+                            </StandardText>
+                            <View style={styles.buttonContainer}><AddEvent /></View>
+                            <View style={selectStyle()}>
+                                <BigText>Next event</BigText>
+                            </View>
+                            <Event key={nextEvent.id} event={nextEvent} expanded />
+                            <StandardText style={styles.textContainer}>
+                                TRK Software Production
+                            </StandardText>
                         </View>
-                        <Event key={nextEvent.id} event={nextEvent} expanded />
-                        <StandardText style={styles.textContainer}>
-                            TRK Software Production
-                        </StandardText>
-                    </View>
-                </ScrollView>
-            </View>
-        </BackgroundImage>
-    );
-}
+                    </ScrollView>
+                </View>
+            </BackgroundImage>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
